88c412153dac6f1fdf40d4227a8ef020b490e592
Daniel Henneberger <git@danielhenneberger.com>
2024-05-12 19:28:36 -0700

    Bump to RC3
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-cli.rb
+++ sqrl-cli.rb
@@ -1,18 +1,18 @@
 class SqrlCli < Formula
   desc "A SQRL compiler"
   homepage "https://datasqrl.com"
-  url "https://sqrl-cli.s3.amazonaws.com/sqrl-cli.jar"
-  version "0.5.0-RC2"
-  sha256 "5aa5cfb8a3ba1f4a52b91dc36999f7e5f54bc4208dd0a1c8021e380a9549a64d"
+  url "https://sqrl-cli.s3.amazonaws.com/sqrl-cli-v0.5.0-RC3.jar"
+  version "0.5.0-RC3"
+  sha256 "39deb3173414bc325f2a5ac36c9644f49e03466db66e43a3d95c84feef047be7"
   license ""
 
   depends_on "openjdk@11"
 
   def install
-    libexec.install "sqrl-cli.jar"
+    libexec.install "sqrl-cli-v0.5.0-RC3.jar"
     (bin/"sqrl").write <<~EOS
       #!/bin/bash
-      exec "#{Formula["openjdk@11"].opt_bin}/java" -jar "#{libexec}/sqrl-cli.jar" "$@"
+      exec "#{Formula["openjdk@11"].opt_bin}/java" -jar "#{libexec}/sqrl-cli-v0.5.0-RC3.jar" "$@"
     EOS
     (bin/"sqrl").chmod 0755
   end
