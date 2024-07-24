87c5f6e2318bbecab9c6239319a839968dcda476
Daniel Henneberger <git@danielhenneberger.com>
2024-07-24 01:05:28 -0700

    0.5.3 release
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-cli.rb
+++ sqrl-cli.rb
@@ -1,23 +1,35 @@
 class SqrlCli < Formula
   desc "A SQRL compiler"
   homepage "https://datasqrl.com"
-  url "https://sqrl-cmd.s3.us-west-2.amazonaws.com/sqrl-cli-v0.5.2.jar"
-  version "0.5.2"
-  sha256 "ad75f153758e4dead31050868c4a15fbf98ced90572f8794ff9145c7eab2ecc0"
+  url "https://sqrl-cmd.s3.us-west-2.amazonaws.com/sqrl-cli-v0.5.3.zip"
+  version "0.5.3"
+  sha256 "a48f30cc8a8dc2ed7ee2867cfe77c1243cdba72836792e2691410a3e538f4b90"
   license ""
 
   depends_on "openjdk@11"
 
   def install
-    libexec.install "sqrl-cli-v0.5.2.jar"
+    system "unzip", "sqrl-cli-v#{version}.zip", "-d", libexec
+
+    # Assuming your ZIP contains sqrl-cli.jar and sqrl-run.jar, move them if necessary
+    libexec.install "sqrl-cli.jar"
+    libexec.install "sqrl-run.jar"
+
     (bin/"sqrl").write <<~EOS
       #!/bin/bash
-      exec "#{Formula["openjdk@11"].opt_bin}/java" -jar "#{libexec}/sqrl-cli-v0.5.2.jar" "$@"
+
+      "#{Formula["openjdk@11"].opt_bin}/java" -jar "#{libexec}/sqrl-cli.jar" "$@"
+      status=$?
+
+      if [ "$1" = "run" ] && [ $status -eq 0 ]; then
+          exec "#{Formula["openjdk@11"].opt_bin}/java" -jar "#{libexec}/sqrl-run.jar" "${@:2}"
+      fi
     EOS
+
     (bin/"sqrl").chmod 0755
   end
-#
-#   test do
-#     system false
-#   end
+
+  test do
+    assert_match "v#{version}", shell_output("#{bin}/sqrl --version")
+  end
 end
