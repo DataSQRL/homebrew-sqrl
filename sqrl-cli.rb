5da140e1a895ae7663582cc45e76606104550882
Daniel Henneberger <git@danielhenneberger.com>
2024-10-29 15:41:07 -0700

    Bump to v0.5.7
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-cli.rb
+++ sqrl-cli.rb
@@ -1,9 +1,9 @@
 class SqrlCli < Formula
   desc "A SQRL compiler"
   homepage "https://datasqrl.com"
-  version "0.5.6"
-  url "https://sqrl-cmd.s3.us-west-2.amazonaws.com/sqrl-cli-v0.5.6.jar", using: :nounzip
-  sha256 "0ff966b2b55ada9b7ddfc22c521e3b0d30b47c87db4e7e9b40f55e1301a4e823"
+  version "0.5.7"
+  url "https://sqrl-cmd.s3.us-west-2.amazonaws.com/sqrl-cli-v0.5.7.jar", using: :nounzip
+  sha256 "905af12fd2d6f912cdf78434edcf917037d53dd79a8694d6182d904880cea368"
   license ""
 
   depends_on "openjdk@11"
@@ -11,10 +11,10 @@ class SqrlCli < Formula
   def install
     # Create the bin directory if it doesn't exist
     bin.mkpath
-    libexec.install "sqrl-cli-v0.5.6.jar" unless File.exist?("#{libexec}/sqrl-cli-v0.5.6.jar")
+    libexec.install "sqrl-cli-v0.5.7.jar" unless File.exist?("#{libexec}/sqrl-cli-v0.5.7.jar")
 
     # Verify that the necessary files are present
-    unless File.exist?("#{libexec}/sqrl-cli-v0.5.6.jar")
+    unless File.exist?("#{libexec}/sqrl-cli-v0.5.7.jar")
       odie "Missing expected files"
     end
 
@@ -32,7 +32,7 @@ class SqrlCli < Formula
         DATASQRL_TOKEN=$(cat "$HOME/.datasqrl/auth")
       fi
       if [[ "$1" == "login" || "$1" == "publish" ]]; then
-        "#{Formula["openjdk@11"].opt_bin}/java" -jar "#{libexec}/sqrl-cli-v0.5.6.jar" "$@"
+        "#{Formula["openjdk@11"].opt_bin}/java" -jar "#{libexec}/sqrl-cli-v0.5.7.jar" "$@"
       else
         # Check if Docker is installed
         if ! command -v docker &> /dev/null; then
@@ -41,7 +41,7 @@ class SqrlCli < Formula
         fi
 
         docker run -it -p 8888:8888 -p 8081:8081 -p 9092:9092 \\
-        --rm -v "$PWD":/build -e DATASQRL_TOKEN="$DATASQRL_TOKEN" datasqrl/cmd:v0.5.6 "$@"
+        --rm -v "$PWD":/build -e DATASQRL_TOKEN="$DATASQRL_TOKEN" datasqrl/cmd:v0.5.7 "$@"
       fi
     EOS
 
